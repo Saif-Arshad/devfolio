38d85874e5bbadc908872a3631e5d9ed649f638d
saif-arshad <saifurrehman.web@gmail.com>
2024-12-22 20:03:47 +0500

    feat: enhance WhatWeDo component with additional messaging and layout improvements

--- app/about-us/_components/what-we-do.tsx
+++ app/about-us/_components/what-we-do.tsx
@@ -3,7 +3,7 @@ function WhatWeDo() {
     return (
         <>
 
-            <div className='px-2 lg:px-5 mt-10' >
+            <div className='px-2 lg:px-10 mt-10' >
 
 
                 <h2
@@ -14,7 +14,44 @@ function WhatWeDo() {
                 >
                     I help founders turn ideas into business-focused digital products
                 </h2>
-            </div>
+                <p className='sm:text-lg mt-10 w-full md:w-11/12 text-gray-200 sm:px-5'>
+                    I assist brands, companies, institutions, and startups in creating exceptional digital experiences for their businesses through strategic development and innovative solutions, ensuring every product aligns with their unique goals and drives measurable success.
+                </p>
+
+                <div className='bg-neutral-800 my-7 mt-14 p-5 rounded-2xl sm:mx-5'>
+
+                    <h2
+                        className="font-display flex items-center gap-x-1 text-2xl text-start font-bold  text-black dark:text-white sm:text-4xl my-1"
+
+                    >
+                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="34" width="34" xmlns="http://www.w3.org/2000/svg" className="snipcss0-2-2-3"><path d="M20.92 2.38A15.72 15.72 0 0 0 17.5 2a8.26 8.26 0 0 0-6 2.06Q9.89 5.67 8.31 7.27c-1.21-.13-4.08-.2-6 1.74a1 1 0 0 0 0 1.41l11.3 11.32a1 1 0 0 0 1.41 0c1.95-2 1.89-4.82 1.77-6l3.21-3.2c3.19-3.19 1.74-9.18 1.68-9.43a1 1 0 0 0-.76-.73zm-2.36 8.75L15 14.67a1 1 0 0 0-.27.9 6.81 6.81 0 0 1-.54 3.94L4.52 9.82a6.67 6.67 0 0 1 4-.5A1 1 0 0 0 9.39 9s1.4-1.45 3.51-3.56A6.61 6.61 0 0 1 17.5 4a14.51 14.51 0 0 1 2.33.2c.24 1.43.62 5.04-1.27 6.93z"></path><circle cx="15.73" cy="8.3" r="2"></circle><path d="M5 16c-2 1-2 5-2 5a7.81 7.81 0 0 0 5-2z"></path></svg>
+                        Let&apos;s work together!
+                    </h2>
+                    <p className='mt-2'>
+                        I&lsquo;m open for freelance projects, feel free to email me to see how can we collaborate.
+                    </p>
+
+                    <div className="flex flex-wrap mt-5">
+                        <a href="https://github.com/Saif-Arshad" target='_blank' rel="noreferrer" className="ml-5 mt-4 hover:text-primaryColor transform hover:scale-110 transition duration-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-6 w-6" aria-hidden="true">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>
+                        </a>
+                        <a href="https://www.linkedin.com/in/saif-rehman-professional/" target='_blank' className="ml-5 mt-4 hover:text-primaryColor transform hover:scale-110 transition duration-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6" aria-hidden="true">
+                                <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                            </svg>
+                        </a>
+
+                        <a href="mailto:saifarshad3344@gmail.com" className="ml-5 mt-4 hover:text-primaryColor transform hover:scale-110 transition duration-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-mail">
+                                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                                <polyline points="22,6 12,13 2,6"></polyline>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div >
         </>
     )
 }
